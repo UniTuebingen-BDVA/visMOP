c484f004dfbd7802abbad505cb26346efe130469
NiBrc <26893899+NiBrc@users.noreply.github.com>
2022-04-05 17:40:52 +0200

    More python unit tests

--- tests/test_reactome_hierarchy.py
+++ tests/test_reactome_hierarchy.py
@@ -1,3 +1,4 @@
+from html import entities
 import pathlib
 import pytest
 
@@ -6,8 +7,8 @@ from visMOP.python_scripts.reactome_hierarchy import PathwayHierarchy
 data_path = pathlib.Path().resolve()
 
 
-@pytest.fixture()
-def hierarchy_setup():
+@pytest.fixture(autouse=True, scope="session")
+def hierarchy():
     """ Generate hierarchy as fixture for the unit tests"""
     target_db = 'MMU'
     reactome_hierarchy = PathwayHierarchy()
@@ -17,15 +18,15 @@ def hierarchy_setup():
 
 
 class TestGetSubtreeTarget:
-    def test_get_subtree_at_leaf(self, hierarchy_setup):
+    def test_at_leaf(self, hierarchy):
         """When already at leaf, subtree should only be the leaf itself"""
         subtree_expected = ['R-MMU-381753']
 
-        assert hierarchy_setup.get_subtree_target('R-MMU-381753') == subtree_expected # check that subtree is only query
+        assert hierarchy.get_subtree_target('R-MMU-381753') == subtree_expected # check that subtree is only query
 
-    def test_get_small_subtree(self, hierarchy_setup):
+    def test_small_subtree(self, hierarchy):
         """test small subtree"""
-        generated_subtree = hierarchy_setup.get_subtree_target('R-MMU-9717189')
+        generated_subtree = hierarchy.get_subtree_target('R-MMU-9717189')
         query_elem_index = generated_subtree.index('R-MMU-9717189')
 
         assert len(generated_subtree) == 3 # check if size is correct
@@ -33,8 +34,8 @@ class TestGetSubtreeTarget:
         assert query_elem_index > generated_subtree.index('R-MMU-9717207') # check if children nodes are correctly placed BEFORE the query node
         assert query_elem_index > generated_subtree.index('R-MMU-9730628') # check if children nodes are correctly placed BEFORE the query node
 
-    def test_get_medium_subtree(self, hierarchy_setup):
-        generated_subtree = hierarchy_setup.get_subtree_target('R-MMU-2187338')
+    def test_medium_subtree(self, hierarchy):
+        generated_subtree = hierarchy.get_subtree_target('R-MMU-2187338')
         query_elem_index = generated_subtree.index('R-MMU-2187338')
 
         assert len(generated_subtree) == 7 # check if size is correct
@@ -47,6 +48,33 @@ class TestGetSubtreeTarget:
         assert generated_subtree.index('R-MMU-2514856') > generated_subtree.index('R-MMU-2485179') # check if children nodes of specific subtree node are placed before it
 
 class TestGetSubtreeNonOverview():
-    def test_subtreeoverview():
-        #TODO
-        assert True
+    def test_at_non_overview(self, hierarchy):
+        """ If already at node which posses a non-overview diagram"""
+        non_overview_diagrams = hierarchy.get_subtree_non_overview('R-MMU-2187338')
+        assert non_overview_diagrams == ['R-MMU-2187338'] # check if result contains only query
+    def test_small_tree(self, hierarchy):
+        """ Not starting at non overview, small testcase """
+        non_overview_diagrams = ['R-MMU-2187338', 'R-MMU-9717189', 'R-MMU-381753']
+        assert set(non_overview_diagrams) == set(hierarchy.get_subtree_non_overview('R-MMU-9709957')) # check if results contain the same pathways
+    def test_skip_node(self, hierarchy):
+        """ Not starting at non overview with node skip, i.e. not the first level of child-nodes posses non-overview diagrams"""
+        non_overview_diagrams = ['R-MMU-5663220', 'R-MMU-5668599', 'R-MMU-5627117', 'R-MMU-5666185', 'R-MMU-5663213', 'R-MMU-5625900', 'R-MMU-5626467', 'R-MMU-5625970', 'R-MMU-5627123', 'R-MMU-5625740', 'R-MMU-5627083', 'R-MMU-9012999', 'R-MMU-9706019', 'R-MMU-9715370']
+        assert set(non_overview_diagrams) == set(hierarchy.get_subtree_non_overview('R-MMU-9716542')) # check if results contain the same pathways
+class TestFindDiagram():
+    def test_already_at_diagram(self, hierarchy):
+        entries = []
+        hierarchy._find_diagram_recursion('R-MMU-9717189', entries, 0)
+        true_diagram_parent = [('R-MMU-9717189', 0)]
+        assert true_diagram_parent == entries
+    
+    def test_simple_example(self, hierarchy):
+        entries = []
+        hierarchy._find_diagram_recursion('R-MMU-9730628', entries, 0)
+        true_diagram_parent = [('R-MMU-9717189', 1)]
+        assert true_diagram_parent == entries
+
+    def test_branch_example(self, hierarchy):
+        entries = []
+        hierarchy._find_diagram_recursion('R-MMU-162699', entries, 0)
+        true_diagram_parents = [('R-MMU-163125', 1), ('R-MMU-446203', 3)]
+        assert set(true_diagram_parents) == set(entries)
