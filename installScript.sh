152d9af3948664f3822a514ef18698f51df2dd3f
NiBrc <26893899+NiBrc@users.noreply.github.com>
2023-03-01 10:42:04 +0100

    new install scripts

--- installScript.sh
+++ installScript.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mkdir uniprot_files session_cache reactome_data reactome_data/pickles
-wget https://stringdb-static.org/download/protein.links.v11.5/10090.protein.links.v11.5.txt.gz
+mkdir session_cache reactome_data reactome_data/pickles
 cd reactome_data
 wget -i ../reactome_file_links.txt
 tar -xf diagram.tgz
